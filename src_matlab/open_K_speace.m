ed6f09fcdaf95d1dfa3bf062a2280f973042e6a4
yj-wang14 <741440734@qq.com>
2016-12-03 17:24:42 +0800

    Add files via upload

--- src_matlab/open_K_speace.m
+++ src_matlab/open_K_speace.m
@@ -2,7 +2,7 @@ function [K,K_info] = open_K_speace(unit_topology_table)
     m = max(max(unit_topology_table));
     K_info = zeros(1,2*m);
     K_info(1) = 1;
-    k_info(2) = 3;
+    K_info(2) = 3;
     for i = 2 : m
         all_line_idx = sum((unit_topology_table == i)')' > 0;
         min_of_A = min(min(unit_topology_table(all_line_idx, :)));
