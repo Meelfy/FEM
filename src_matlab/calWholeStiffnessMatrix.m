902ea6e8811a09bbf27d5e731ecd8ff18c4e65cf
yj-wang14 <741440734@qq.com>
2016-12-03 16:49:35 +0800

    Add files via upload

--- src_matlab/calWholeStiffnessMatrix.m
+++ src_matlab/calWholeStiffnessMatrix.m
@@ -1,4 +1,4 @@
-function K = calWholeStiffnessMatrix(unit_topology_table)
+function [K,K_info] = calWholeStiffnessMatrix(Coord, unit_topology_table, Materials,cal_type)
 % 这个函数计算整体的劲度矩阵K
 % 因为计算方法未定，输入部分的说明自行完成
 % 输入：单元劲度阵
@@ -9,3 +9,9 @@ function K = calWholeStiffnessMatrix(unit_topology_table)
 %     如果K的储存不是矩阵，请使用cell储存
 %     并给出每个cell的含义
 %     之后参数的传递以 K 为准
+    open_K_speace(unit_topology_table);
+    n = size(unit_topology_table,1);
+    for i = 1:n
+        matrixB = calMatrixB(element_X, element_Y);
+        matrixD = calMatrixD(Materials(i,1), Materials(i,2), cal_type);
+        element_k = calElementStiffnessMatrix(matrixB, matrixD, t, Area(element_X, element_Y));
--- src_matlab/open_K_speace.m
+++ src_matlab/open_K_speace.m
@@ -1,2 +1,11 @@
-function K = open_K_speace(unit_info)
-    
+function [K,K_info] = open_K_speace(unit_topology_table)
+    m = max(max(unit_topology_table));
+    K_info = zeros(1,m);
+    K_info(1) = 1;
+    for i = 2 : m
+        all_line_idx = sum((unit_topology_table == i)')' > 0;
+        min_of_A = min(min(unit_topology_table(all_line_idx, :)));
+        nozero = i - min_of_A + 1;
+        K_info(i) = K_info(i-1) + nozero;
+    end
+    K = zeros(1,K_info(m));
