487e6d6004d5b806e5670e4eb759352d888f92e9
MajorChina <535370561@qq.com>
2016-12-03 21:09:26 +0800

    build sparse K

---
+++ src_matlab/calWholeStiffnessMatrixSparse.m
@@ -0,0 +1,30 @@
+function K = calWholeStiffnessMatrixSparse(coord, unit_topology_table, materials,cal_type)
+% 这个函数计算整体的劲度矩阵K
+% 因为计算方法未定，输入部分的说明自行完成
+% 输入：单元劲度阵
+% 
+% 输出：
+%     整体刚度矩阵 K
+%     K 为 n * n 的矩阵 的某种储存形式
+%     如果K的储存不是矩阵，请使用cell储存
+%     并给出每个cell的含义
+%     之后参数的传递以 K 为准
+n = max(unit_topology_table(:));
+K = sparse(2*n, 2*n);
+t = 1;
+for i = 1:n
+    element_X = coord(unit_topology_table(i, :)', 1);
+    element_Y = coord(unit_topology_table(i, :)', 2);
+    matrixB = calMatrixB(element_X, element_Y);
+    matrixD = calMatrixD(materials(i,1), materials(i,2), cal_type);
+    element_k = calElementStiffnessMatrix(matrixB, matrixD, t, calAera(element_X, element_Y));
+    element_k = sparse(element_k);
+
+    position = zeros(1,6);
+    for ii = 1:3
+        position(ii*2-1) = unit_topology_table(i,ii)*2-1;
+        position(ii*2) = unit_topology_table(i,ii)*2;
+    end
+
+    K(position, position) = K(position, position) + element_k;
+end
--- src_matlab/open_K_speace.m
+++ src_matlab/open_K_speace.m
@@ -1,13 +1,13 @@
 function [K,K_info] = open_K_speace(unit_topology_table)
-    m = max(max(unit_topology_table));
-    K_info = zeros(1,2*m);
+    n = max(max(unit_topology_table));
+    K_info = zeros(1,2*n);
     K_info(1) = 1;
     K_info(2) = 3;
-    for i = 2 : m
+    for i = 2 : n
         all_line_idx = sum((unit_topology_table == i)')' > 0;
         min_of_A = min(min(unit_topology_table(all_line_idx, :)));
         nozero = i - min_of_A + 1;
         K_info(2*i-1) = K_info(2*i-2) + nozero*2-1;
         K_info(2*i) = K_info(2*i-1) + nozero*2;
     end
-    K = zeros(1,K_info(2*m));
+    K = zeros(1,K_info(2*n));
