bbe3b7b19044461d09d0bcb592e8e9a7f6a6f597
dodoright <13141345243@163.com>
2016-12-03 21:53:34 +0800

    Add files via upload

---
+++ input/sam1.0/processForce.m
@@ -0,0 +1,19 @@
+function P = processForce(P, K, bound)
+% 这个函数对P进行处理，使其满足约束
+% 输入：
+%      外力荷载P
+% 输出：
+%      处理后的P
+Ak = K{1}; % line
+Ind = K{2}; % 2 * n
+
+
+for i = 1 : size(bound,1);
+	elementNum = bound(i, 1);
+	XorY = bound(i, 2);
+	j = elementNum * 2 - 2 + XorY;
+	displacement = bound(i, 3);
+
+	% K(i,j) 相当于 Ak(Ind(i) - (i - j))
+	P(j) = Ak(Ind(j)) * (1 / eps) * displacement;
+end 
