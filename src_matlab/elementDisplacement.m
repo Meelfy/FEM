45cf441eee10205bcb20d97ecf9949ce8bbf69a3
Huanghf123 <2590512691@qq.com>
2016-12-03 22:08:22 +0800

    add element displacement cal

--- src_matlab/elementDisplacement.m
+++ src_matlab/elementDisplacement.m
@@ -12,3 +12,16 @@ function element_displacement = elmentDisplacement(whole_d, element_x, elment_y)
 
 % 此时单元节点位移已经可以整理后输出为成果，可以在此直接打印
 % 注意打印格式
+n = size(unit_topology_table,1);
+element_displaycement = zeros(n,6)
+for i = 1:n
+    position = zeros(1, 6);
+    for ii = 1:3
+        position(ii*2 - 1) = unit_topology_table(i,ii)*2 - 1;
+        position(ii*2) = unit_topology_table(i,ii)*2;
+    end
+    for ii = 1:6
+        element_displaycement(i,ii) = whole_d(position(ii));
+    end
+end
+dlmwrite('output/elementdisplaycement.dat', element_displaycement);
