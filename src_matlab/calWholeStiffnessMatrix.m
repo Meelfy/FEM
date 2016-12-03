f48bed531aa5fae3449a25d9ec6da9190f6450ae
zhangjh14 <842289414@qq.com>
2016-12-03 18:56:55 +0800

    Add files via upload

--- src_matlab/calWholeStiffnessMatrix.m
+++ src_matlab/calWholeStiffnessMatrix.m
@@ -1,4 +1,4 @@
-function [K,K_info] = calWholeStiffnessMatrix(Coord, unit_topology_table, Materials,cal_type)
+function [K,K_info] = calWholeStiffnessMatrix(coord, unit_topology_table, materials,cal_type)
 % 这个函数计算整体的劲度矩阵K
 % 因为计算方法未定，输入部分的说明自行完成
 % 输入：单元劲度阵
@@ -13,21 +13,25 @@ function [K,K_info] = calWholeStiffnessMatrix(Coord, unit_topology_table, Materi
     n = size(unit_topology_table,1);
     t = 1;
     for i = 1:n
-        element_X = Coord(unit_topology_table(i)',1);
-        element_Y = Coord(unit_topology_table(i)',2);
+        element_X = coord(unit_topology_table(i, :)',1);
+        element_Y = coord(unit_topology_table(i, :)',2);
         matrixB = calMatrixB(element_X, element_Y);
-        matrixD = calMatrixD(Materials(i,1), Materials(i,2), cal_type);
-        element_k = calElementStiffnessMatrix(matrixB, matrixD, t, Area(element_X, element_Y));
+        matrixD = calMatrixD(materials(i,1), materials(i,2), cal_type);
+        element_k = calElementStiffnessMatrix(matrixB, matrixD, t, calAera(element_X, element_Y));
         position = zeros(1,6);
         for ii = 1:3
-            position(ii*2-1) = unit_topology_table(i)*2-1;
-            position(ii*2) = unit_topology_table(i)*2;
+            position(ii*2-1) = unit_topology_table(i,ii)*2-1;
+            position(ii*2) = unit_topology_table(i,ii)*2;
         end
+        position
         for j = 1:6
             for jj = 1:6
+                if position(j) >= position(jj)
                 oned_x = K_info(position(j)) - (position(j)-position(jj));
                 K(oned_x) = K(oned_x) + element_k(j,jj);
+                end
             end
         end
     end
+
     
