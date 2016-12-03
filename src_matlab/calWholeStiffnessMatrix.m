447afff52e76dbe421a2baac52aa44a1000d95e2
yj-wang14 <741440734@qq.com>
2016-12-03 14:39:23 +0800

    Add files via upload

--- src_matlab/calWholeStiffnessMatrix.m
+++ src_matlab/calWholeStiffnessMatrix.m
@@ -1,7 +1,7 @@
-function K = calWholeStiffnessMatrix()
+function K = calWholeStiffnessMatrix(ke，position_v,K)
 % 这个函数计算整体的劲度矩阵K
 % 因为计算方法未定，输入部分的说明自行完成
-% 输入：
+% 输入：单元劲度阵
 % 
 % 输出：
 %     整体刚度矩阵 K
@@ -9,3 +9,8 @@ function K = calWholeStiffnessMatrix()
 %     如果K的储存不是矩阵，请使用cell储存
 %     并给出每个cell的含义
 %     之后参数的传递以 K 为准
+for i = 1:6
+    for j = 1:6
+        K(position_v(i),position_v(j)) = K(position_v(i),position_v(j)) + ke(i,j);
+    end
+end
